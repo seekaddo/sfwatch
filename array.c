8a185484024c8b6ff2415572eb1b1efc0848e0d4
seekaddo <dennis_addo@aol.com>
2017-07-28 20:25:38 +0200

    modified with a more classified version

--- array.c
+++ array.c
@@ -12,58 +12,83 @@
 #include "array.h"
  
  
-static size_t initsize = 5;
-static size_t current_size = 0;
- 
-Array init_string(size_t size) {
-     
-    if (size != initsize) {
-        initsize = size;
-    }
-    Array str = malloc(size * sizeof(char *));
-    if (str == NULL) {
-        fprintf(stderr, "malloc: %s\n", strerror(errno));
+
+sds_array init_sds_array(size_t size){
+    sds_array sda;
+    
+    
+    sda.mem = malloc(size * sizeof(char *));
+    if(sda.mem == NULL){
+        fprintf(stderr,"malloc: %s\n",strerror(errno));
         exit(EXIT_FAILURE);
     }
-    memset(str, 0, size);
-    return str;
+    memset(sda.mem,0,size);
+    sda.capacity = size;
+    sda.size = 0;
+    return sda;
 }
- 
-void add_str(Array *str, const char *s) {
-    if (*str == NULL) {
-        printf("Please init str first\n");
+
+
+void add_str(sds_array *sdsa,const char *s){
+    if(sdsa->mem == NULL){
+        printf("Please init sdsa first\n");
         return;
     }
-    static size_t l = 0;
-     
-    if (l == initsize) {
-        initsize *= 2;
-        *str = realloc(*str, initsize * sizeof(char *));
-        if (*str == NULL) {
-            fprintf(stderr, "realloc: %s\n", strerror(errno));
+    static size_t l = 0;     // using l is not classic but just for easy readability as sda->mem[sda->size]
+    
+    if(l == sdsa->capacity){
+        sdsa->capacity *= 2;
+        sdsa->mem = realloc(sdsa->mem,sdsa->capacity * sizeof(char *));
+        if(sdsa->mem == NULL){
+            fprintf(stderr,"realloc: %s\n",strerror(errno));
             exit(EXIT_FAILURE);
         }
     }
-     
-    size_t len = strlen(s) + 1;
-     
-    (*str)[l] = malloc(len * sizeof(char));
-    snprintf((*str)[l], len, "%s", s);
-    current_size = l;
-    l++;
-     
-     
-}
- 
-size_t get_size(void) {
-    return (current_size != 0 && current_size != initsize) ? current_size + 1 : initsize;
-}
- 
- 
-void freeall(Array *ss, size_t len) {
-    for (size_t i = 0; (*ss)[i] != NULL && i < len; ++i) {
-        free((*ss)[i]);
-        (*ss)[i] = NULL;
+    
+    size_t len = strlen(s)+1;
+    
+    sdsa->mem[l] = malloc(len * sizeof(char));
+    if(sdsa->mem[l] == NULL){
+        fprintf(stderr,"malloc: %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
     }
-    free(*ss);
+    snprintf(sdsa->mem[l],len,"%s",s);
+    sdsa->size = ++l;
+    
+    
 }
+
+
+void delete_str(sds_array *array, const size_t index){
+    
+    if(array->mem == NULL){
+        fprintf(stderr,"delete_str: sds_array is empty.!\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    if(index < array->size){
+        free(array->mem[index]);
+    } else{
+        fprintf(stderr,"delete_str: Index out of range\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    //resizing the elements is Big O n O(n) when element is index = 0
+    // is a pain but will keep things in order.
+    for (size_t i = index; i < array->size ; ++i) {
+        
+        array->mem[i] =  i != array->size-1  ? array->mem[i+1]: NULL;
+
+    }
+    
+    array->size -= 1;
+    
+}
+
+void freeall(sds_array *sds,size_t len){
+    for (size_t i = 0; sds->mem[i] != NULL && i <len ; ++i) {
+        free(sds->mem[i]);
+    }
+    free(sds->mem);
+}
+
--- array.h
+++ array.h
@@ -1,6 +1,8 @@
 /**
  *@file string.h
- *  A simple Array datatype to store array of strings
+ *  A simple dynamic string  array for storing strings
+ 	using struct for a compact datatype and remove the tripple star programming
+ 	This sd_array is tracks the size and capactiy for easy access.
  *  @author Dennis Addo
  *  @date 24/07/2017
  **/
@@ -11,16 +13,20 @@
 #include <stdio.h>
  
  
-typedef char **Array;
+typedef struct sd_array {
+    size_t capacity;        //holds the total size of the array
+    size_t size;            //holds the current number of elements in the array
+    char **mem;             //where elements are stored
+}sds_array;
  
  
-extern Array init_string(size_t size);
+extern sds_array init_sds_array(size_t size);
  
-extern size_t get_size(void);
+extern void add_str(sds_array *sdsa,const char *s);
  
-extern void add_str(Array *str, const char *s);
+extern void delete_str(sds_array *array, const size_t index);
  
-extern void freeall(Array *ss, size_t len);
+extern void freeall(sds_array *sds,size_t len);
  
  
 #endif //ARRAY_H
